9fa80bc9fba2a8a16d7c723fd226d6fb9f7df163
Andi DeVois <adevois@gmail.com>
2017-05-20 13:28:16 -0500

    data-binding for add function

--- Todo/app/addController.js
+++ Todo/app/addController.js
@@ -6,6 +6,17 @@
 
     function addController($scope) {
         $scope.welcome = "Add controller works";
+
+
+        //you don't need to pre-define properties, but the object needs to exist
+        $scope.todo = {
+            name: "",
+            isComplete: false
+        };
+
+        $scope.submit = function(todo) {
+            console.log(todo);
+        }
     }
-    
+
 })();

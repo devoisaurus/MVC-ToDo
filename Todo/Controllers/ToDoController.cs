7d659c93ca2393e5b2c30a5fd91943681eff2977
Andi DeVois <adevois@gmail.com>
2017-05-20 12:50:28 -0500

    Starting MVC controller

---
+++ Todo/Controllers/ToDoController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Todo.Controllers
+{
+    public class ToDoController : ApiController
+    {
+        [Route("/todo")]
+        public List<string> GetAll()
+        {
+            return new List<string> { "hello", "my", "name", "is", "todo"};
+        }
+    }
+}
